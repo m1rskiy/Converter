d383e0b2bf7b122a8b7bd784cb8506217fa00fb8
user <user@DESKTOP-O9IN8D3>
2023-01-28 21:07:42 +0100

    Добавьте файлы проекта.

---
+++ Converter/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int a;
+
+	cout << "Enter EUR: " << endl;
+	cin >> a;
+
+	cout << "Your EUR into UAH = " << a * 40.14 << endl;
+
+	cout << "Your EUR into USD = " << a * 0.92 << endl;
+}
